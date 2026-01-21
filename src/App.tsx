b9c386efdc893867af999fea059b05919f36b6fd
GgeraA <142055522+GgeraA@users.noreply.github.com>
2026-01-21 13:29:06 -0600

    configuracion inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Empresa de Soluciones Tecnológicas
+      </h1>
+    </div>
+  );
+}
+
+export default App;
